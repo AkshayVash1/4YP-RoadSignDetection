42acffee65c00a136323a79853a4fb9c28532600
Akshay <akshayvashisht15@gmail.com>
2024-02-22 01:45:03 -0500

    new change_label.py. removal of old dataset, added new dataset for traffic lights.

---
+++ change_label.py
@@ -0,0 +1,31 @@
+import os
+
+dir_path = r"C:\Users\Akshay\.vscode\4YP-RoadSignDetection\4YP-RoadSignDetection\traffic-light\train\labels"
+save_path = r"C:\Users\Akshay\.vscode\4YP-RoadSignDetection\4YP-RoadSignDetection\saves"
+
+res = []
+
+for path in os.listdir(dir_path):
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+
+for file in res:
+    with open(dir_path + "\\" + file, 'r') as rf:
+            lines = rf.readlines()
+            rf.close()
+            if not lines:
+                print("FILE IS EMPTY\n")
+                print("Filename: " + file + "\n")
+            else:
+                os.chdir(save_path)
+                with open(file, "w") as wf:
+                    for line in lines:
+                        if line[0] == "0":
+                            newline = line.replace("0", "3")
+                            wf.write(newline)
+                            wf.close()
+                        else: 
+                            wf.write(line)
+                            wf.close()
+                            
+                    
